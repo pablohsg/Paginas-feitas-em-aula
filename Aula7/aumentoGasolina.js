5b88beec1188131f1f5d3132b1359c166d662f31
pablohsg <101648776+pablohsg@users.noreply.github.com>
2022-06-04 08:54:32 -0300

    Add files via upload

---
+++ Aula7/aumentoGasolina.js
@@ -0,0 +1,4 @@
+let antigo_valor = prompt("Digite o penultimo valor da gasolina:");
+let atual_valor = prompt("Digite o atual valor d agasolina:");
+let diferenca = atual_valor - antigo_valor;
+alert(`O valor da gasolina aumentou ${diferenca}`);
---
+++ Aula7/notas.js
@@ -0,0 +1,6 @@
+let prova1 = parseInt(prompt("Digite a nota da prova 1:"));
+let prova2 = parseInt(prompt("Digite a nota da prova 2:"));
+let trabalho = parseInt(prompt("Digite a nota do trabalho: "));
+let notaFinal = prova1 + prova2 + trabalho;
+
+alert(`Sua nota final foi: ${notaFinal}!`)
